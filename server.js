fbf494efdd740a911f6ce35cfccb3e111283d6fd
Chanantipat <chanantipat08@gmail.com>
2026-07-16 21:03:49 +0700

    Fix server route paths

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const cors = require("cors");
-const workoutRoutes = require("./routes/workoutRoutes");
-const userRoutes = require("./routes/userRoutes");
-const workoutHistoryRoutes = require("./routes/workoutHistoryRoutes");
+
+const workoutRoutes =
+    require("./server/routes/workoutRoutes");
+
+const userRoutes =
+    require("./server/routes/userRoutes");
+
+const workoutHistoryRoutes =
+    require("./server/routes/workoutHistoryRoutes");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/history", workoutHistoryRoutes);
+
 app.get("/", (req, res) => {
     res.json({
         message: "Welcome to FitQuest REST API"
     });
 });
+
 const PORT = 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(
+        `Server running on http://localhost:${PORT}`
+    );
 });
